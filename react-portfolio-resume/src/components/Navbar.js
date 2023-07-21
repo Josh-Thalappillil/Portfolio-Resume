2c874e6a0746356bbddb89034664628982e2048d
Josh <jthalappillil7@gmail.com>
2023-07-21 15:11:29 +1000

    Fixed fonts and colors in CSS and added font families. Integrated Navbar and changed scheme. Fixed scaling and size of drop down menu which now appears when window width is below 960px.

--- react-portfolio-resume/src/components/Navbar.js
+++ react-portfolio-resume/src/components/Navbar.js
@@ -1,66 +1,92 @@
-import React, {useState, useEffect} from 'react';
-import {Link} from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import './Navbar.css';
-
-
+import { Button } from './Button.js';
 
 function Navbar() {
     const [click, setClick] = useState(false);
-    const [button, setButton] = useState(true);
+    const [windowWidth, setWindowWidth] = useState(window.innerWidth);
 
     const navClick = () => setClick(!click);
     const closeMenu = () => setClick(false);
 
-    const showButton = () => {
-        if (window.innerWidth <= 960) {
-        setButton(false);
-        } else {
-        setButton(true);
-        }
+    const updateWindowWidth = () => {
+        setWindowWidth(window.innerWidth);
     };
-    
 
     useEffect(() => {
-        showButton()
+        window.addEventListener('resize', updateWindowWidth);
+        return () => window.removeEventListener('resize', updateWindowWidth);
     }, []);
 
-    window.addEventListener('resize', showButton);
-  return (
-    <>
+    // Function to handle click on the hamburger menu icon
+    const handleMenuClick = () => {
+        setClick(!click);
+    };
+        
+    return (
+        <>
         <nav className="navbar">
             <div className="navbar-container">
-                <Link to="/" className="navbar-logo">
-                    Josh Thalappillil <i className="fa-solid fa-code"/>
-                </Link>
-                {/* <div className="menu-icon" onClick = {navClick}>
+            <Link to="/" className="navbar-logo">
+                Josh Thalappillil <span className='logo'><i className="fa-solid fa-code"/></span>
+            </Link>
+            {windowWidth <= 960 ? (
+                <div className="menu-icon" onClick={handleMenuClick}>
                 <i className={click ? 'fas fa-times' : 'fas fa-bars'}></i>
-                </div> */}
-                <ul className = {click ? 'nav-menu active' : 'nav-menu'}>
+                </div>
+            ) : (
+                <ul className="nav-menu">
                 <li className='nav-item'>
                     <Link to='/' className='nav-links' onClick={closeMenu}>
-                        Home
+                    Home
                     </Link>
                 </li>
                 <li className='nav-item'>
                     <Link to='/about' className='nav-links' onClick={closeMenu}>
-                        About
+                    About
                     </Link>
                 </li>
                 <li className='nav-item'>
                     <Link to='/projects' className='nav-links' onClick={closeMenu}>
-                        Projects
+                    Projects
                     </Link>
                 </li>
                 <li className='nav-item'>
                     <Link to='/contact' className='nav-links' onClick={closeMenu}>
-                        Contact
+                    Contact
                     </Link>
                 </li>
                 </ul>
+            )}
             </div>
         </nav>
-    </>
-  )
+        {click && windowWidth <= 960 && (
+            <ul className="nav-menu dropdown">
+            <li className='nav-item'>
+                <Link to='/' className='nav-links' onClick={closeMenu}>
+                Home
+                </Link>
+            </li>
+            <li className='nav-item'>
+                <Link to='/about' className='nav-links' onClick={closeMenu}>
+                About
+                </Link>
+            </li>
+            <li className='nav-item'>
+                <Link to='/projects' className='nav-links' onClick={closeMenu}>
+                Projects
+                </Link>
+            </li>
+            <li className='nav-item'>
+                <Link to='/contact' className='nav-links' onClick={closeMenu}>
+                Contact
+                </Link>
+            </li>
+            </ul>
+        )}
+        </>
+    );
 }
 
-export default Navbar
+export default Navbar;
