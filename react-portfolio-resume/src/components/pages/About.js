e48ed32bb55294179b519efe6f89704a8d508605
Josh <jthalappillil7@gmail.com>
2023-07-22 23:57:19 +1000

    Removal of stock images and addition of new image plus change of CSS. Addition of about me content and Technical Skills heading.

--- react-portfolio-resume/src/components/pages/About.js
+++ react-portfolio-resume/src/components/pages/About.js
@@ -1,12 +1,33 @@
 import React from 'react';
 import '../../App.css';
-import './pages.css';
+import './about.css';
 
 function About() {
   return (
     <div className='about-container'>
-        <h1>About Me</h1>
+      <div className='about-content'>
+        <div className='about-header'>
+          <h1>About Me</h1>
+          <i className="fa-solid fa-gamepad fa-beat"></i>
+        </div>
         <div className='underline'></div>
+      </div>
+      <div className='about-contents'>
+        <div className='about-text'>
+          <h1>Hello, my name is Josh.</h1>
+          <p>I am a passionate software developer and recent graduate with a hunger for innovation.<br/>
+            Skilled in diverse programming languages and committed to continuous learning.
+            Collaborative<br/> team player ready to make a positive impact through technology. 
+            Excited to contribute my<br/> skills to challenging projects and thrive in dynamic environments.</p>
+        </div>
+        <div className='about-image'>
+          <img src="/images/about-me.jpg" alt="aboutMeImg" />
+        </div>
+      </div>
+      <div className='techSkills'>
+        <h1>Technical Skills</h1>
+        <div className='underline'></div>
+      </div>
     </div>
   );
 }
